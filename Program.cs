152ca914a3493e5944e4d7f47e38b4c32a129f79
denys.holovin <denys.holovin@cgi.com>
2018-12-14 18:02:27 -0400

    update

--- Program.cs
+++ Program.cs
@@ -26,26 +26,62 @@ namespace CoinsOnClock
             Console.WriteLine($"Slots: { string.Join(",", slots)}");
             Console.WriteLine($"Choices: { string.Join(",", choices)}");
             Console.WriteLine($"Position History: { string.Join(",", positionHistory)}");
-            
-            Console.WriteLine(GetRemainingCoinWeightsSum());
 
+            Console.WriteLine(GetSumOfRemainingCoinWeights());
         }
 
-        static void CalculateCoinWeightForClockPosition(int currentPosition)
+        static void CalculateCoinWeightForClockPosition(int currentPosition, int startCoinWeightIndex = 0)
         {
-            if (GetRemainingCoinWeightsSum() == 0)
+            if (GetSumOfRemainingCoinWeights() == 0)
                 return;
 
-            foreach (var coinWeight in coinWeights)
+            for (var coinWeightIndex = startCoinWeightIndex; coinWeightIndex < coinWeights.Length; coinWeightIndex++)
+            // foreach (var coinWeight in coinWeights)
             {
-                if (reachedCoinWeightUsageLimit(choices, coinWeight))
-                    continue; // We can't use more than certain amount of Coins with the same Weight
+                var coinWeight = coinWeights[coinWeightIndex];
 
-                if (isSlotOccupied(currentPosition, coinWeight))
-                    continue; // if occupied - decrease weight 10 -> 5 -> 1 and try again
-                //else if (isSlotOccupied(currentPosition, GetRemainingCoinWeightsSum()))
-                    // TODO: try to move on without this validation
-                    // continue; // if occupied - decrease weight 10 -> 5 -> 1 and try again
+                // We can't use more than certain amount of Coins with the same Weight
+                // if occupied - decrease weight 10 -> 5 -> 1 and try again
+                if (ReachedCoinWeightUsageLimit(choices, coinWeight) || isSlotOccupied(currentPosition, coinWeight))
+                {
+                    if (coinWeight != coinWeights.Min())
+                        continue;
+                    else
+                    {
+                        // Handle the case when we have gone through all coinWeights, but couldn't find a way to proceed:
+                        // get back to previous Choice, decrease coinWeight and repeat Iteration for that Slot until we go further
+                        // foreach (var prevPosition in reversedPositionHistory)
+                        var lastPositionHistoryIndex = positionHistory.Count() - 1;
+                        for (var idx = lastPositionHistoryIndex; idx == 0; idx--)
+                        {
+                            var prevPosition = positionHistory[idx];
+                            var prevPositionCoinWeight = choices[prevPosition - 1]; // {10 | 5 | 1}
+
+                            slots[prevPosition - 1] = false;
+                            choices[prevPosition - 1] = 0;
+                            positionHistory.RemoveAt(idx);
+
+                            if (prevPositionCoinWeight == coinWeights.Min())
+                                // can't decrease CoinWeight - go back one more step and try to decrease coin weight there
+                                continue;
+                            else
+                            {
+                                var prevSlotCoinWeightIdx = Array.IndexOf(coinWeights, prevPositionCoinWeight);
+
+                                if (prevSlotCoinWeightIdx == -1)
+                                    throw new Exception(
+                                        $"Couldnt find coinWeight {prevPositionCoinWeight} in array {string.Join(",", coinWeights)}");
+
+                                // Decrease CoinWeight on the previous step and retry
+                                CalculateCoinWeightForClockPosition(prevPosition, prevSlotCoinWeightIdx + 1);
+                                //getNextPosition(prevPosition, coinWeights[prevSlotCoinWeightIdx]));
+                            }
+                        }
+                    }
+                    // else if (isSlotOccupied(currentPosition, GetSumOfRemainingCoinWeights()))
+                    //     // TODO: try to move on without this validation
+                    //     continue; // if occupied - decrease weight 10 -> 5 -> 1 and try again
+                }
                 else
                 {
                     // Save CoinWeight choice for the current Slot                    
@@ -58,40 +94,11 @@ namespace CoinsOnClock
                 }
             }
 
-            // Handle the case when we have gone through all coinWeights, but couldn't find a way to proceed:
-            // get back to previous Choice, decrease coinWeight and repeat Iteration for that Slot until we go further
-            // foreach (var prevPosition in reversedPositionHistory)
-            var lastPositionHistoryIndex =  positionHistory.Count() - 1;
-            for (var idx = lastPositionHistoryIndex; idx >= 0; idx--)
-            {
-                var prevPosition = positionHistory[idx];
-                var prevPositionCoinWeight = choices[prevPosition - 1]; // {10 | 5 | 1}
-
-                slots[prevPosition - 1] = false;
-                choices[prevPosition - 1] = 0;
-                positionHistory.RemoveAt(idx);
-
-                if (prevPositionCoinWeight == coinWeights.Min())
-                    // can't decrease CoinWeight - go back one more step and try to decrease coin weight there
-                    continue;
-                else
-                {
-                    var prevSlotCoinWeightIdx = Array.IndexOf(coinWeights, prevPositionCoinWeight);
-                    
-                    if (prevSlotCoinWeightIdx == -1)
-                        throw new Exception(
-                            $"Couldnt find coinWeight {prevPositionCoinWeight} in array {string.Join(",", coinWeights)}");
-                    
-                    // Decrease CoinWeight on the previous step and retry
-                    CalculateCoinWeightForClockPosition(
-                        getNextPosition(prevPosition, coinWeights[prevSlotCoinWeightIdx]));
-                }
-            }
-
-            throw new Exception("No Answer Was Identified");
+            // return;// throw new Exception("No Answer Was Identified");
         }
 
-        static int GetRemainingCoinWeightsSum() {
+        static int GetSumOfRemainingCoinWeights()
+        {
             // return coinWeights.Aggregate((result, coinWeight) => result + coinWeight) * 4 - choices.Sum();
             return 64 - choices.Sum();
         }
@@ -114,7 +121,7 @@ namespace CoinsOnClock
             // return Array.IndexOf(slots, newSlotPosition) != -1;
         }
 
-        static bool reachedCoinWeightUsageLimit(int[] choices, int coinWeight)
+        static bool ReachedCoinWeightUsageLimit(int[] choices, int coinWeight)
         {
             return choices.Count(choice => choice == coinWeight) >= 4;
         }
