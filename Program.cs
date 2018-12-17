db3b14f2dea93049beb10803452ac89c6c2c9620
admin <dholovin@swamp.local>
2018-12-16 23:53:42 -0400

    works for 1 -> 5 -> 10

--- Program.cs
+++ Program.cs
@@ -6,81 +6,81 @@ namespace CoinsOnClock
 {
     class Program
     {
-        // TODO: consider combining slots and choices, if possible  
-        static bool[] slots = new bool[12];
-        static int[] choices = new int[12];
-        static List<int> positionHistory = new List<int>();
-        static int[] coinWeights = new int[] { 10, 5, 1 };
-
-        // static int remainingCoinWeightsSum =>
-        //     (coinWeights.Aggregate((result, coinWeight) => result + coinWeight) * 4)
-        //     - choices.Aggregate((result, choice) => result + choice);
-
-
-
+        static int[] coinWeights = new int[] { 1, 5, 10 };
+        static int maxSlotsCount = 12;
         static Dictionary<int, int> coinsOnClock = new Dictionary<int, int>();
         static int iteration = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Analysis is started...");
-
             PutCoinsOnClock(1);
         }
 
         static void LogState()
         {
+            Console.WriteLine($"Iteration: {iteration}");
             Console.WriteLine($"Slots: { string.Join(",", coinsOnClock.Keys)}");
             Console.WriteLine($"Choices: { string.Join(",", coinsOnClock.Values)}");
-            Console.WriteLine(GetSumOfRemainingCoinWeights());
+            Console.WriteLine($"Remaining Coins: { GetSumOfRemainingCoinWeights()}");
+            Console.WriteLine($"\n\r");
         }
 
         static void PutCoinsOnClock(int currentPosition, int startCoinWeightIndex = 0)
         {
-            LogState();
-
-            Console.WriteLine($"Iteration: {iteration}");
-            iteration += 1;
-
-            if (coinsOnClock.Count == 12)
-                return;
-
-            // loop over coinWeights (10 -> 5 -> 1)
-            for (var coinWeightIndex = startCoinWeightIndex; coinWeightIndex < coinWeights.Length; coinWeightIndex++)
+            // while (iteration != 474)
+            while (coinsOnClock.Count() < maxSlotsCount)
             {
-                LogState();
+                iteration += 1;
 
-                var coinWeight = coinWeights[coinWeightIndex];
-
-                // We can't use more than certain amount of Coins with the same Weight
-                // if occupied - decrease weight 10 -> 5 -> 1 and try again
-                if (ReachedCoinWeightUsageLimit(coinWeight) || isNextSlotOccupied(currentPosition, coinWeight))
+                if (currentPosition == maxSlotsCount)
                 {
-                    if (coinWeight != coinWeights.Min())
-                        continue;
+                  coinsOnClock.Add(currentPosition, GetSumOfRemainingCoinWeights());  
+                  break;
+                }
 
-                    // Handle the case when we have gone through all coinWeights, but couldn't find a way to proceed:
-                    // get back to previous Choice, decrease coinWeight and repeat Iteration until we go further
-                    int prevPosition = 0, prevPositionCoinWeight = 0;
-                    GoBack(out prevPosition, out prevPositionCoinWeight);
-                   
-                    while (prevPositionCoinWeight == coinWeights.Min())
+                // loop over coinWeights (1 -> 5 -> 10)
+                for (var coinWeightIndex = startCoinWeightIndex; coinWeightIndex < coinWeights.Length; coinWeightIndex++)
+                {
+                    var coinWeight = coinWeights[coinWeightIndex];
+
+                    // We can't use more than certain amount of Coins with the same Weight
+                    // if occupied - decrease weight 1 -> 5 -> 10 and try again
+                    if (ReachedCoinWeightUsageLimit(coinWeight) || isNextSlotOccupied(currentPosition, coinWeight))
                     {
-                        // if coin weight on prev step was '1' - can't decrease - GoBack again
+                        if (coinWeight != coinWeights.Max())
+                            continue;
+
+                        // Handle the case when we have gone through all coinWeights, but couldn't find a way to proceed:
+                        // get back to previous Choice, decrease coinWeight and repeat Iteration until we go further
+                        int prevPosition = 0, prevPositionCoinWeight = 0;
                         GoBack(out prevPosition, out prevPositionCoinWeight);
+
+                        while (prevPositionCoinWeight == coinWeights.Max())
+                        {
+                            GoBack(out prevPosition, out prevPositionCoinWeight);
+                        }
+
+                        var prevPositionCoinWeightIdx = Array.IndexOf(coinWeights, prevPositionCoinWeight);
+                        // PutCoinsOnClock(prevPosition, prevPositionCoinWeightIdx + 1);
+
+                        currentPosition = prevPosition;
+                        startCoinWeightIndex = prevPositionCoinWeightIdx + 1;
+                    }
+                    else
+                    {
+                        // Save CoinWeight choice for the current Slot  
+                        coinsOnClock.Add(currentPosition, coinWeight);
+                        // Move on to new position after CoinWeight is applied
+                        // PutCoinsOnClock(getNextPosition(currentPosition, coinWeight));
+                        currentPosition = getNextPosition(currentPosition, coinWeight);
+                        startCoinWeightIndex = 0;
                     }
 
-                    var prevPositionCoinWeightIdx = Array.IndexOf(coinWeights, prevPositionCoinWeight);
-                    PutCoinsOnClock(prevPosition, prevPositionCoinWeightIdx + 1);
+                    break;
                 }
-                else
-                {
-                    // Save CoinWeight choice for the current Slot  
-                    coinsOnClock.Add(currentPosition, coinWeight);
-                    // Move on to new position after CoinWeight is applied
-                    PutCoinsOnClock(getNextPosition(currentPosition, coinWeight));
-                }
-
             }
+
+            LogState();
         }
 
         static void GoBack(out int prevPosition, out int prevPositionCoinWeight)
@@ -97,9 +97,9 @@ namespace CoinsOnClock
 
         static int getNextPosition(int currentPosition, int coinWeight)
         {
-            var newSlotPosition = (currentPosition + coinWeight) % 12;
+            var newSlotPosition = (currentPosition + coinWeight) % maxSlotsCount;
             if (newSlotPosition == 0)
-                newSlotPosition = 12;
+                newSlotPosition = maxSlotsCount;
 
             return newSlotPosition;
         }
