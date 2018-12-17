00f8da553a44e4a70a85b44bebb9453a215d8a4b
denys.holovin <denys.holovin@cgi.com>
2018-12-17 14:41:42 -0400

    use recursion

--- Program.cs
+++ Program.cs
@@ -8,12 +8,72 @@ namespace CoinsOnClock
     {
         static int[] coinWeights = new int[] { 1, 5, 10 };
         static Dictionary<int, int> coinsOnClock = new Dictionary<int, int>();
-        static int iteration = 1;
+        static int iteration = 1; // for logging purposes only
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Analysis is started...");
-            PutCoinsOnClock(1);
+            int startPosition = 1;
+            PutCoinsOnClock(startPosition);
+            LogState();
             Console.WriteLine("Analysis is complete...");
+        }
+
+        static void PutCoinsOnClock(int currentPosition, int startCoinWeightIndex = 0)
+        {
+            iteration += 1;
+
+            // Reconsider this condition if plan to support random flow instead of going from 1 -> 12
+            if (currentPosition == 12)
+            {
+                coinsOnClock.Add(currentPosition, GetSumOfRemainingCoinWeights());
+                return;
+            }
+
+            // loop over coinWeights (1 -> 5 -> 10)
+            for (var coinWeightIndex = startCoinWeightIndex; coinWeightIndex < coinWeights.Length; coinWeightIndex++)
+            {
+                var coinWeight = coinWeights[coinWeightIndex];
+
+                // We can't use more than certain amount of Coins with the same Weight
+                // if occupied - decrease weight 1 -> 5 -> 10 and try again
+                if (ReachedCoinWeightUsageLimit(coinWeight) || isNextSlotOccupied(currentPosition, coinWeight))
+                {
+                    if (coinWeight != coinWeights.Last())
+                        continue;
+ 
+                    // Handle the case when we have gone through all coinWeights, but couldn't find a way to proceed:
+                    // get back to previous Choice, decrease coinWeight and repeat Iteration until we go further
+                    int prevPosition = 0, prevPositionCoinWeight = 0;
+                    GoBack(out prevPosition, out prevPositionCoinWeight);
+
+                    while (prevPositionCoinWeight == coinWeights.Last())
+                    {
+                        GoBack(out prevPosition, out prevPositionCoinWeight);
+                    }
+
+                    var prevPositionCoinWeightIdx = Array.IndexOf(coinWeights, prevPositionCoinWeight);
+
+                    // currentPosition = prevPosition;
+                    // startCoinWeightIndex = prevPositionCoinWeightIdx + 1;
+                    PutCoinsOnClock(prevPosition, prevPositionCoinWeightIdx + 1);
+                }
+                else
+                {
+                    // Save CoinWeight choice for the current Slot  
+                    coinsOnClock.Add(currentPosition, coinWeight);
+                    // Move on to new position after CoinWeight is applied
+                    PutCoinsOnClock(getNextPosition(currentPosition, coinWeight));
+                    // currentPosition = getNextPosition(currentPosition, coinWeight);
+                    // startCoinWeightIndex = 0;
+                }
+
+                // break;
+            }
+            // }
+
+
         }
 
         static void LogState()
@@ -25,62 +85,6 @@ namespace CoinsOnClock
             Console.WriteLine($"\n\r");
         }
 
-        static void PutCoinsOnClock(int currentPosition, int startCoinWeightIndex = 0)
-        {
-            while (coinsOnClock.Count() < 12)
-            {
-                iteration += 1;
-
-                if (currentPosition == 12)
-                {
-                  coinsOnClock.Add(currentPosition, GetSumOfRemainingCoinWeights());  
-                  break;
-                }
-
-                // loop over coinWeights (1 -> 5 -> 10)
-                for (var coinWeightIndex = startCoinWeightIndex; coinWeightIndex < coinWeights.Length; coinWeightIndex++)
-                {
-                    var coinWeight = coinWeights[coinWeightIndex];
-
-                    // We can't use more than certain amount of Coins with the same Weight
-                    // if occupied - decrease weight 1 -> 5 -> 10 and try again
-                    if (ReachedCoinWeightUsageLimit(coinWeight) || isNextSlotOccupied(currentPosition, coinWeight))
-                    {
-                        if (coinWeight != coinWeights.Max())
-                            continue;
-
-                        // Handle the case when we have gone through all coinWeights, but couldn't find a way to proceed:
-                        // get back to previous Choice, decrease coinWeight and repeat Iteration until we go further
-                        int prevPosition = 0, prevPositionCoinWeight = 0;
-                        GoBack(out prevPosition, out prevPositionCoinWeight);
-
-                        while (prevPositionCoinWeight == coinWeights.Max())
-                        {
-                            GoBack(out prevPosition, out prevPositionCoinWeight);
-                        }
-
-                        var prevPositionCoinWeightIdx = Array.IndexOf(coinWeights, prevPositionCoinWeight);
-                        // PutCoinsOnClock(prevPosition, prevPositionCoinWeightIdx + 1);
-
-                        currentPosition = prevPosition;
-                        startCoinWeightIndex = prevPositionCoinWeightIdx + 1;
-                    }
-                    else
-                    {
-                        // Save CoinWeight choice for the current Slot  
-                        coinsOnClock.Add(currentPosition, coinWeight);
-                        // Move on to new position after CoinWeight is applied
-                        // PutCoinsOnClock(getNextPosition(currentPosition, coinWeight));
-                        currentPosition = getNextPosition(currentPosition, coinWeight);
-                        startCoinWeightIndex = 0;
-                    }
-
-                    break;
-                }
-            }
-
-            LogState();
-        }
 
         static void GoBack(out int prevPosition, out int prevPositionCoinWeight)
         {
